9e90b7fc1c1f8f190db467df3537ec4152b33407
harsh-vish14 <harshkv9322@gmail.com>
2021-04-22 21:41:52 +0530

    new patterns added

--- arra.py
+++ arra.py
@@ -1,7 +1,17 @@
-def printing(num):
-    print(num, end=" ")
-
-
-a = int(input("> "))
-for x in range(a + 1):
-    printing(x)
+total = 100
+#country = "US"
+country = "AU"
+if country == "US":
+    if total <= 50:
+        print("Shipping Cost is  $50")
+elif total <= 100:
+        print("Shipping Cost is $25")
+elif total <= 150:
+	    print("Shipping Costs $5")
+else:
+        print("FREE")
+if country == "AU":
+	  if total <= 50:
+	    print("Shipping Cost is  $100")
+else:
+	    print("FREE")
